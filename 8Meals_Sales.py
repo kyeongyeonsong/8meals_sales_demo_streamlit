cbcf89946e4ec9da3658975882fb102615aa22ad
Kyeongyeon Song <kyeongyeon_song@eightmeals.com>
2023-09-22 17:00:23 +0900

    change data source

--- 8Meals_Sales.py
+++ 8Meals_Sales.py
@@ -25,21 +25,22 @@ conn = connect(credentials=credentials)
 
 def convert_to_dict(row):
     return {
-        '상품코드': row.상품코드,
-        '상품명': row.상품명,
         '매장': row.매장,
         '대분류': row.대분류,
         '중분류': row.중분류,
         '소분류': row.소분류,
-        '판매가': row.판매가
+        '상품코드': row.상품코드,
+        '상품명': row.상품명,
+        '매출': row.매출
     }
 
 # Perform SQL query on the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
+@st.cache_data(ttl=60)
 def run_query(query):
     rows = conn.execute(query, headers=1)
     rows = rows.fetchall()
+    print(rows[0])
     return [convert_to_dict(row) for row in rows]
 
 sheet_url = st.secrets["private_gsheets_url"]
@@ -53,7 +54,7 @@ total_sales_by_category = defaultdict(int)
 
 for row in rows:
     category = row['대분류']
-    price = row['판매가']
+    price = row['매출']
     total_sales_by_category[category] += price
 
 
@@ -65,11 +66,11 @@ total_sales_by_subcategory = defaultdict(int)
 
 for row in rows:
     subcategory = row['중분류']
-    price = row['판매가']
+    price = row['매출']
     total_sales_by_subcategory[subcategory] += price
 
 # Convert the defaultdict to a DataFrame for plotting
-plot_data = pd.DataFrame(list(total_sales_by_subcategory.items()), columns=['중분류', '판매가'])
+plot_data = pd.DataFrame(list(total_sales_by_subcategory.items()), columns=['중분류', '매출'])
 
 # Create the bar chart in Streamlit
-st.bar_chart(plot_data.set_index('중분류'))
+st.bar_chart(plot_data.set_index('소분류'))
